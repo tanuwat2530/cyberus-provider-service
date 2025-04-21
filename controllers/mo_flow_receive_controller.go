000e3318e7653a5177b558202fb15f78b420a834
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-04-21 22:30:01 +0700

    Redirect to AOC page

--- controllers/mo_flow_receive_controller.go
+++ controllers/mo_flow_receive_controller.go
@@ -3,18 +3,33 @@ package controllers
 import (
 	"CyberusGolangShareLibrary/utilities"
 	services "cyberus/provider-service/services"
+	"fmt"
 
 	"net/http"
 )
 
 func MoFlowReceive(w http.ResponseWriter, r *http.Request) {
 	// Check if the method is POST
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	response := services.MoFlowReceiveProcessRequest(r)
+	// fmt.Println(response["code"])
+	// fmt.Println(response["partner_id"])
+	// fmt.Println(response["refid"])
+	// fmt.Println(response["adsid"])
+	//utilities.ResponseWithJSON(w, http.StatusOK, response)
 
-	utilities.ResponseWithJSON(w, http.StatusOK, response)
+	// Build redirect URL with query params
+	if response["code"] == "302" {
+		target := fmt.Sprintf("https://portal.mmtcontent.com/landing?id=%s&refid=%s&media=%s", response["partner_id"], response["refid"], response["adsid"])
+		// Send redirect (302 by default)
+		http.Redirect(w, r, target, http.StatusFound)
+	}
+	if response["code"] == "-1" {
+		utilities.ResponseWithJSON(w, http.StatusOK, response)
+	}
+
 }
--- services/mo_flow_receive_service.go
+++ services/mo_flow_receive_service.go
@@ -4,122 +4,159 @@ import (
 	"CyberusGolangShareLibrary/redis_db"
 	"encoding/json"
 	"log"
+	"net/http"
 	"strconv"
 	"time"
-
-	"fmt"
-	"net/http"
 
 	"github.com/google/uuid"
 )
 
 // Struct to map the expected JSON fields
-type MoFlowReceiveRequest struct {
-	AgencyId  string `json:"agency_id"`
-	PartnerId string `json:"partner_id"`
-	RefId     string `json:"refid"`
-	AdsId     string `json:"adsid"`
-}
+// type MoFlowReceiveRequest struct {
+// 	AgencyId  string `json:"agency_id"`
+// 	PartnerId string `json:"partner_id"`
+// 	RefId     string `json:"refid"`
+// 	AdsId     string `json:"adsid"`
+// }
 
 func MoFlowReceiveProcessRequest(r *http.Request) map[string]string {
 
+	// var payload map[string]interface{}
 	res := map[string]string{}
-	var payload map[string]interface{}
-	// Get current time
+
+	// // Get current time
 	now := time.Now()
-	// Unix timestamp in nanoseconds
+	// // Unix timestamp in nanoseconds
 	timestamp := (now.UnixNano())
 	nano_timestamp := strconv.FormatInt(timestamp, 10)
 
-	// Generate a random UUID (UUID v4)
+	// // Generate a random UUID (UUID v4)
 	transaction_id := uuid.New().String()
 
-	// Get a Client IP address
-	ip := r.RemoteAddr
+	// // Get a Client IP address
+	client_ip := r.RemoteAddr
 
-	errPayload := json.NewDecoder(r.Body).Decode(&payload)
-	if errPayload != nil {
-		// Example: print the values
-		//fmt.Println("Error decode Json to map[string]interface{} :", errPayload.Error())
+	agency_id := r.URL.Query().Get("agency_id")
+	partner_id := r.URL.Query().Get("partner_id")
+	refid := r.URL.Query().Get("refid")
+	adsid := r.URL.Query().Get("adsid")
 
+	if agency_id == "" || partner_id == "" || refid == "" || adsid == "" {
 		res["code"] = "-1"
-		res["desc"] = "JSON Decode error"
-		res["ref-id"] = "undefined"
-		res["tran-ref"] = "undefined"
-		res["media"] = "undefined"
+		res["message"] = "Invalid param"
 		return res
 	}
 
-	jsonData, err := json.MarshalIndent(payload, "", "  ")
-	if err != nil {
-		//fmt.Println("Error marshalling JSON:", err.Error())
-		res["code"] = "-2"
-		res["desc"] = "JSON Marshalling error"
-		res["ref-id"] = "undefined"
-		res["tran-ref"] = "undefined"
-		res["media"] = "undefined"
-		return res
+	// Parse query parameters into a map
+	payload := map[string]string{
+		"agency_id":  agency_id,
+		"partner_id": partner_id,
+		"refid":      refid,
+		"adsid":      adsid,
 	}
 
-	// // Unmarshal JSON into struct
-	var requestData MoFlowReceiveRequest
-	err = json.Unmarshal(jsonData, &requestData)
-	if err != nil {
-		//fmt.Println("Error map Json to Struct :" + err.Error())
-		//fmt.Println("Error marshalling JSON:", err.Error())
-		res["code"] = "-3"
-		res["desc"] = "JSON Struct error"
-		res["ref-id"] = "undefined"
-		res["tran-ref"] = "undefined"
-		res["media"] = "undefined"
-		return res
-	}
-
-	// // Add ClientIP to Payload
-	payload["client_ip"] = ip
+	// ✅ Add a new JSON node (e.g., tracking_source)
+	payload["client_ip"] = client_ip
 	payload["timestamp"] = nano_timestamp
 	payload["transaction_id"] = transaction_id
-	payload["agency_id"] = requestData.AgencyId
-	payload["partner_id"] = requestData.PartnerId
-	payload["ads_id"] = requestData.AdsId
-	payload["ref_id"] = requestData.RefId
-	// Convert the struct to JSON string
-	payloadBytes, err := json.Marshal(payload)
+
+	// Convert to JSON
+	jsonData, err := json.Marshal(payload)
 	if err != nil {
-		fmt.Printf("Failed to convert payload to JSON:  %+v\n ", http.StatusInternalServerError)
-		fmt.Println("Error marshalling JSON:", err.Error())
-		res["code"] = "-4"
-		res["desc"] = "Additional value error"
+		//http.Error(w, "Failed to convert to JSON", http.StatusInternalServerError)
+		res["code"] = "-1"
+		res["message"] = "Json error"
+		res["transaction_id"] = transaction_id
 		return res
 	}
 
-	payloadString := string(payloadBytes)
-	fmt.Println(payloadString)
-	redis_db.ConnectRedis()
-	redis_key := "MO:" + requestData.PartnerId + ":" + transaction_id
+	payloadString := string(jsonData)
+	//fmt.Println(payloadString)
 
-	ttl := 1 * time.Hour // expires in 1 Hour
+	redis_db.ConnectRedis()
+	redis_key := "MO:" + partner_id + ":" + transaction_id
+
+	ttl := 240 * time.Hour // expires in 240 Hour
 
 	// Set key with TTL
 	if err := redis_db.SetWithTTL(redis_key, payloadString, ttl); err != nil {
 		//write to file if Redis problem or forward request to AIS
 		log.Fatalf("SetWithTTL error: %v", err)
 	}
-	fmt.Println("Key set successfully with TTL")
+	//fmt.Println("Key set successfully with TTL")
 
-	// Get the key
-	// val, err := redis_db.GetValue(key)
-	// if err != nil {
-	// 	log.Printf("GetValue error: %v", err)
-	// } else {
-	// 	fmt.Printf("Retrieved value: %s\n", val)
+	// errPayload := json.NewDecoder(r.Body).Decode(&payload)
+	// if errPayload != nil {
+	// 	// Example: print the values
+	// 	//fmt.Println("Error decode Json to map[string]interface{} :", errPayload.Error())
+	// 	res["code"] = "-1"
+	// 	res["desc"] = "JSON Decode error"
+	// 	res["ref-id"] = "undefined"
+	// 	res["tran-ref"] = "undefined"
+	// 	res["media"] = "undefined"
+	// 	return res
 	// }
 
-	//redis_db.Set("aaa", "AAA", 300)
+	// jsonData, err := json.MarshalIndent(payload, "", "  ")
+	// if err != nil {
+	// 	//fmt.Println("Error marshalling JSON:", err.Error())
+	// 	res["code"] = "-2"
+	// 	res["desc"] = "JSON Marshalling error"
+	// 	res["ref-id"] = "undefined"
+	// 	res["tran-ref"] = "undefined"
+	// 	res["media"] = "undefined"
+	// 	return res
+	// }
 
-	res["code"] = "200"
-	res["message"] = "OK"
-	res["transaction_id"] = transaction_id
+	// // // Unmarshal JSON into struct
+	// var requestData MoFlowReceiveRequest
+	// err = json.Unmarshal(jsonData, &requestData)
+	// if err != nil {
+	// 	//fmt.Println("Error map Json to Struct :" + err.Error())
+	// 	//fmt.Println("Error marshalling JSON:", err.Error())
+	// 	res["code"] = "-3"
+	// 	res["desc"] = "JSON Struct error"
+	// 	res["ref-id"] = "undefined"
+	// 	res["tran-ref"] = "undefined"
+	// 	res["media"] = "undefined"
+	// 	return res
+	// }
+
+	// // // Add ClientIP to Payload
+	// payload["client_ip"] = ip
+	// payload["timestamp"] = nano_timestamp
+	// payload["transaction_id"] = transaction_id
+	// payload["agency_id"] = requestData.AgencyId
+	// payload["partner_id"] = requestData.PartnerId
+	// payload["ads_id"] = requestData.AdsId
+	// payload["ref_id"] = requestData.RefId
+	// // Convert the struct to JSON string
+	// payloadBytes, err := json.Marshal(payload)
+	// if err != nil {
+	// 	fmt.Printf("Failed to convert payload to JSON:  %+v\n ", http.StatusInternalServerError)
+	// 	fmt.Println("Error marshalling JSON:", err.Error())
+	// 	res["code"] = "-4"
+	// 	res["desc"] = "Additional value error"
+	// 	return res
+	// }
+
+	// payloadString := string(payloadBytes)
+	// fmt.Println(payloadString)
+	// redis_db.ConnectRedis()
+	// redis_key := "MO:" + requestData.PartnerId + ":" + transaction_id
+
+	// ttl := 240 * time.Hour // expires in 240 Hour
+
+	// // Set key with TTL
+	// if err := redis_db.SetWithTTL(redis_key, payloadString, ttl); err != nil {
+	// 	//write to file if Redis problem or forward request to AIS
+	// 	log.Fatalf("SetWithTTL error: %v", err)
+	// }
+	// fmt.Println("Key set successfully with TTL")
+	res["code"] = "302"
+	res["partner_id"] = partner_id
+	res["refid"] = refid
+	res["adsid"] = adsid
 
 	defer r.Body.Close()
 
