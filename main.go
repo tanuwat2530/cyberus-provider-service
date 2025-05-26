7d5a456e7d4992ecb9ceadf5dcde21249b1d364d
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-05-26 19:00:07 +0700

    Change start port to 4001

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	routes.SetupRoutes()
 
 	// Start the server on port 8080
-	fmt.Println("Starting cyberus-provider-service server on port 8081...")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+	fmt.Println("Starting cyberus-provider-service server on port 4001...")
+	if err := http.ListenAndServe(":4001", nil); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 
